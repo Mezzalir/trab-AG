7a6619ff629fd0a1a9863a7c18457cdbcd07e82a
Lucas Willian Mezzalira <mezzaliramezzalira22@gmail.com>
2026-09-26 00:08:48 -0300

    Adiciona ranking e probabilidade de seleção
    - Cromossomo ganha os atributos rank e probabilidade
    - Populacao.atribuir_ranking: ordena e dá rank N ao melhor e 1 ao pior
    - Populacao.calcular_probabilidades: P = rank / (N(N+1)/2)
    - imprimir_populacao mostra rank e probabilidade em %
    - main.py calcula as probabilidades da população inicial

--- cromossomo.py
+++ cromossomo.py
@@ -1,16 +1,18 @@
 from criar_individuo import criar_individuo, codificar, decodificar
 
+
 class Cromossomo:
     def __init__(self):
         self.genes = criar_individuo()
         self.fitness = self.calcular_fitness()
+        self.rank = 0
+        self.probabilidade = 0
 
     def calcular_fitness(self):
         x = decodificar(self.genes[:5])
         y = decodificar(self.genes[5:])
-        self.fitness = 2*x**2 - 13*x + x*y - 7*y/3
+        self.fitness = 2 * x**2 - 13 * x + x * y - 7 * y / 3
 
         return self.fitness
 
     # def mutacao(self):
-
--- populacao.py
+++ populacao.py
@@ -13,7 +13,32 @@ class Populacao:
 
     def imprimir_populacao(self):
         for individuo in self.populacao:
-            print(f"indiviuo = {individuo.genes}  fitness = {individuo.fitness}")
+            print(
+                f"individuo = {individuo.genes}  fitness = {individuo.fitness}  rank = {individuo.rank}  prob = {individuo.probabilidade:.2%}"
+            )
 
     def ordenar_populacao(self):
-        self.populacao = sorted(self.populacao, key=lambda crom: crom.fitness, reverse=True)
+        self.populacao = sorted(
+            self.populacao, key=lambda crom: crom.fitness, reverse=True
+        )
+
+    def atribuir_ranking(self):
+        # garante que o melhor esta no indice 0
+        self.ordenar_populacao()
+        # len devolve a quantidade de itens da lista e é guardando em N
+        N = len(self.populacao)
+
+        # o loop fará N interacoes . indice vai de 0 ate  N - 1 porque a contagem comeca em 0
+        # melhor individuo recebe N. O segundo melhor N - 1 e assim vai , tendeu
+        for indice, individuo in enumerate(self.populacao):
+            individuo.rank = N - indice
+
+    def calcular_probabilidades(self):
+        self.atribuir_ranking()
+        N = len(self.populacao)
+
+        # soma dos ranks (formula)
+        soma_ranks = N * (N + 1) / 2
+
+        for individuo in self.populacao:
+            individuo.probabilidade = individuo.rank / soma_ranks
