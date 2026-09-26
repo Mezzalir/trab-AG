5b02d8629f0dd3a6aff1d4719060708864980e8b
Lucas Willian Mezzalira <mezzaliramezzalira22@gmail.com>
2026-09-26 00:08:48 -0300

    Formata criar_individuo.py

--- criar_individuo.py
+++ criar_individuo.py
@@ -1,11 +1,12 @@
 import random
 from code_e_decode import codificar, decodificar
 
+
 def criar_individuo():
 
-        x = random.randint(-15, 15)
-        y = random.randint(-15, 15)
+    x = random.randint(-15, 15)
+    y = random.randint(-15, 15)
 
-        individuo = codificar(x) + codificar(y)
+    individuo = codificar(x) + codificar(y)
 
-        return individuo
+    return individuo
